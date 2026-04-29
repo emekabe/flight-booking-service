36626e061aaa7db68ad8393f8fdfc85c1c1ea2f8
Emeka Chukwulobe <emekabe@yahoo.com>
2026-04-29 15:50:12 +0100

    Fixed liquibase configuration

---
+++ src/main/java/com/emekachukwulobe/flightbookingservice/config/LiquibaseConfig.java
@@ -0,0 +1,20 @@
+package com.emekachukwulobe.flightbookingservice.config;
+
+import javax.sql.DataSource;
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LiquibaseConfig {
+
+    @Bean
+    public SpringLiquibase liquibase(DataSource dataSource) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:db/changelog/db.changelog-master.yaml");
+        liquibase.setDataSource(dataSource);
+        liquibase.setShouldRun(true);
+        // This ensures Liquibase runs before Hibernate tries to validate the schema
+        return liquibase;
+    }
+}
